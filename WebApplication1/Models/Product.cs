8df4eb0512c9e53e997258e53972366a24ac7d09
Fahimsz <chowdhury.sfahim@gmail.com>
2026-09-14 20:54:34 +0600

    Models updated for Products and database linked at appsettings

---
+++ WebApplication1/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
